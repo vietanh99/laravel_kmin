45d21ff7308c60e85ab6ef11510783931f0f3ecd
PhamVietAnh99 <40080810+PhamVietAnh99@users.noreply.github.com>
2020-03-24 10:15:54 +0700

    first commit

---
+++ vietanh/routes/web.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/hello', function(){
+    return view('hello', ['name' => 'Kmin']);
+    });
+    Route::group(['prefix'=>'/admin'],function(){
+        Route::get('/danhsachnghiviec','adminController@danhsachnghiviec');
+        Route::get('/phongban','adminController@phongban');
+        Route::get('/chucvu','adminController@chucvu');
+        Route::get('/bangcap','adminController@bangcap');
+        ...
+        });
+        Route::post(‘/nhanvien’,nhanvienController@create)
+        {
+        });
+        Route::post(‘/nhanvien/{id}’,nhanvienController@edit)
+        {
+        });
+        Route::delete(‘/nhanvien/{id}’,nhanvienController@destroy)
+        {
+        });
+        Route::post(‘/nhanvien/{id}’,nhanvienController@update)
+        {
+        });
+
+    trangchu.blade.php 
+    nhanvien.blade.php 
+    admin.blade.php 
+    danhsachnhanvien.blade.php 
+    phongban.blade.php 
+    bangcap.blade.php 
+    chucvu.blade.php 
+    danhsachnghiviec.blade.php 
+    ... 
+
+
+
